f3ab6515820c4ba5f1a8f45953eae5cd912c94cd
mmcdon20 <mmcdon20@live.com>
2013-12-31 04:50:49 -0600

    Increased the efficiency for problem 21

--- src/Euler/Euler021.scala
+++ src/Euler/Euler021.scala
@@ -17,13 +17,13 @@ Evaluate the sum of all the amicable numbers under 10000.
 object Euler021 extends App {
 
   def d(n: Int) =
-    (1 until n).filter(n % _ == 0).sum
+    (1 to n/2).filter(n % _ == 0).sum
 
   def amicable(a: Int)(b: Int = d(a)) =
     d(a) == b && d(b) == a && a != b
 
-  var total =
-    (1 until 10000).filter(amicable(_)( )).sum
+  val total =
+    (1 until 10000).par.filter(amicable(_)( )).sum
 
   println(total)
 }
