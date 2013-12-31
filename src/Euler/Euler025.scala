14d00bcea3c023814a17b83f2d62f15b7aec800c
mmcdon20 <mmcdon20@live.com>
2013-12-31 00:45:05 -0600

    Changed implementation of solution 25

--- src/Euler/Euler025.scala
+++ src/Euler/Euler025.scala
@@ -3,11 +3,7 @@ package Euler
 import Util.Common._
 
 object Euler025 extends App {
-  var n = 1
-  
-  while (fib(n).toString.length < 1000) {
-    n += 1
-  }
-  
-  println(n)
+  def count(n: Int = 1): Int =
+    if (fib(n).toString.length < 1000) count(n+1) else n
+  println(count())
 }
