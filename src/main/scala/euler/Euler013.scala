b68bf6146222118652ad2da88b607e7368c65a21
mmcdon20 <mmcdon20@live.com>
2014-01-04 06:26:05 -0600

    fixed format for euler 13 output

--- src/main/scala/euler/Euler013.scala
+++ src/main/scala/euler/Euler013.scala
@@ -5,5 +5,5 @@ import scala.io.Source
 object Euler013 extends App {
   val file = Source.fromFile("src/main/resources/13.txt").getLines
   val numbers = file.map(BigInt(_))
-  println(numbers sum)
+  println(numbers.sum.toString.take(10))
 }
