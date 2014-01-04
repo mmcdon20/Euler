67684c450ff676b8b08305e56c8188e0f662a930
mmcdon20 <mmcdon20@live.com>
2014-01-04 06:07:53 -0600

    simplified problem 8

--- src/main/scala/euler/Euler008.scala
+++ src/main/scala/euler/Euler008.scala
@@ -6,20 +6,6 @@ import util.Implicit._
 object Euler008 extends App {
   val file = Source.fromFile("src/main/resources/8.txt")
   val nums = file.toList.filter(_.isDigit).map(_.toDigit)
-    
-  // solution 1
-  def greatestProduct(xs:List[Int], p:Int):Int = xs match {
-    case a::b::c::d::e::fs =>
-      if (a*b*c*d*e > p)
-        greatestProduct(b::c::d::e::fs, a*b*c*d*e)
-      else
-        greatestProduct(b::c::d::e::fs, p)
-    case _ => p
-  }
-    
-  // solution 2
   val gp = nums.sliding(5).map(_.product).max
-    
-  println(greatestProduct(nums, 0))
   println(gp)
 }
