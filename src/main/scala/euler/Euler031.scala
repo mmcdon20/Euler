b9ffd6278e55e6c83d57f5ecaaf009c56bd91759
mmcdon20 <mmcdon20@live.com>
2014-01-04 06:22:17 -0600

    simplified problem 31

--- src/main/scala/euler/Euler031.scala
+++ src/main/scala/euler/Euler031.scala
@@ -1,22 +1,9 @@
 package euler
 
 object Euler031 extends App {
-  // with for comprehension
-  val x = for {
-    a <- 200 to 0 by -200
-    b <- a to 0 by -100
-    c <- b to 0 by -50
-    d <- c to 0 by -20
-    e <- d to 0 by -10
-    f <- e to 0 by -5
-    g <- f to 0 by -2
-  } yield (a,b,c,d,e,f,g)
-  println (x length)
-
-  // with dynamic programming
   var coins = List(1,2,5,10,20,50,100,200)
   var table = Array.ofDim[Int](201)
-  table(0) = 1 // base case
+  table(0) = 1
 
   for (i <- 0 until coins.length; j <- coins(i) until 201)
     table(j) += table(j - coins(i))
