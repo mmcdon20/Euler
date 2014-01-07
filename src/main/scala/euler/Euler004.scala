acadabeb22738b206812cd50b3429b7e1458288f
mmcdon20 <mmcdon20@live.com>
2014-01-06 20:49:00 -0600

    simplified problem 4

--- src/main/scala/euler/Euler004.scala
+++ src/main/scala/euler/Euler004.scala
@@ -3,10 +3,7 @@ package euler
 import util.Implicit._
 
 object Euler004 extends App {
-  var xs:List[Int] = Nil
-   
-  for (i <- 100 to 999; j <- 100 to 999; if ((i*j).isPalindrome))
-    xs = (i*j) :: xs
-    
-  println(xs max)
+  val items = for (i <- 100 to 999; j <- 100 to 999; if ((i*j).isPalindrome))
+    yield i*j
+  println(items max)
 }
