491f6154e2e4a3b58ca10e3afe68bef8698dd0eb
mmcdon20 <mmcdon20@live.com>
2014-01-10 23:57:33 -0600

    solution for problem 29

---
+++ src/main/scala/euler/Euler035.scala
@@ -0,0 +1,15 @@
+package euler
+
+import util.Implicit._
+
+object Euler035 extends App {
+  def rotations(n: Int) =
+    (0 until n.toString.length)
+      .map(i => (n.toString.drop(i) + n.toString.take(i)).toInt)
+
+  def isCircularPrime(n: Int) =
+    !rotations(n).exists(!_.isPrime)
+
+  val result = (1 until 1000000).par.count(isCircularPrime(_))
+  println(result)
+}
