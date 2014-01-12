55b81b810eb8e06828245c0380b1c8ee3f109209
mmcdon20 <mmcdon20@live.com>
2014-01-12 02:16:56 -0600

    improved solution to problem 37

--- src/main/scala/euler/Euler037.scala
+++ src/main/scala/euler/Euler037.scala
@@ -3,20 +3,12 @@ package euler
 import util.Implicit._
 
 object Euler037 extends App {
-  
-  def isTruncatablePrime(n:BigInt):Boolean = {
-    var num1 = n
-    var num2 = n
-    while (num1.toString.length > 1) {
-      if (!num1.isPrime || !num2.isPrime)
-        return false
-      num1 = num1.toString.drop(1).toInt
-      num2 = num2.toString.dropRight(1).toInt
-    }
-    num1.isPrime && num2.isPrime
-  }
-  
-  val x = (10 to 1000000).par.filter(isTruncatablePrime(_)).sum
+  def isTruncatablePrime(n: Int): Boolean =
+    !(1 until n.toString.length).exists { i =>
+      !(n.toString.drop(i).toInt.isPrime &&
+        n.toString.take(i).toInt.isPrime)
+    } && n.isPrime
+
+  val x = (10 to 1000000).filter(isTruncatablePrime(_)).sum
   println(x)
-  
 }
