5abd4e0fad98258d2337b1cc8166f68ad7ab81f8
mmcdon20 <mmcdon20@live.com>
2014-01-12 00:59:03 -0600

    improved efficiency of problem 9

--- src/main/scala/euler/Euler009.scala
+++ src/main/scala/euler/Euler009.scala
@@ -1,7 +1,11 @@
 package euler
 
 object Euler009 extends App {
-  for (a <- 1 to 1000; b <- 1 to 1000; c <- 1 to 1000)
-    if ((a < b && b < c) && (a*a + b*b == c*c) && (a + b + c == 1000))
-      println(a*b*c)
+  def triplet() : Int = {
+    for (a <- 1 to 1000; b <- 1 to 1000; c <- (1 to 1000))
+      if ((a < b && b < c) && (a*a + b*b == c*c) && (a + b + c == 1000))
+        return a*b*c
+    return 0
+  }
+  println(triplet)
 }
