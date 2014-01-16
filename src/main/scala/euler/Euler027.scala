e1375ed4c3106936a3690edb0b39ce5876df359e
mmcdon20 <mmcdon20@live.com>
2014-01-15 22:48:59 -0600

    added solution for problem 27

---
+++ src/main/scala/euler/Euler027.scala
@@ -0,0 +1,22 @@
+package euler
+
+import util.Implicit._
+import scala.math.abs
+
+object Euler027 extends App {
+
+  def formula (a:Int, b:Int, n:Int) =
+    abs((n*n + a*n + b)).isPrime
+
+  def comboLength(a:Int, b:Int, n:Int = 0): Int =
+    if (formula(a,b,n)) comboLength(a,b,n+1)
+    else n
+
+  val combos =
+    for (a <- -999 to 999; b <- -999 to 999)
+      yield (comboLength(a,b),a*b)
+
+  val result = combos.par.maxBy(_._1)
+
+  println(result._2)
+}
