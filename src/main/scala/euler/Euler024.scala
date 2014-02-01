1f158cae580504f65f90ca508f638f7a70302932
mmcdon20 <mmcdon20@live.com>
2014-01-31 21:09:09 -0600

    updated variable name in problem 24

--- src/main/scala/euler/Euler024.scala
+++ src/main/scala/euler/Euler024.scala
@@ -1,6 +1,6 @@
 package euler
 
 object Euler024 extends App {
-  val permutaions = "0123456789".permutations
-  println(permutaions.drop(999999).next)
+  val permutations = "0123456789".permutations
+  println(permutations.drop(999999).next)
 }
