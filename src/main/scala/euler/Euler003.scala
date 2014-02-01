e30357564811034891058386e654ccbbaa82c1a8
mmcdon20 <mmcdon20@live.com>
2014-01-31 23:03:58 -0600

    added primes to util common, updated problem 3

--- src/main/scala/euler/Euler003.scala
+++ src/main/scala/euler/Euler003.scala
@@ -1,10 +1,7 @@
 package euler
 
+import util.Common.primes
+
 object Euler003 extends App {
-  def largest(n: BigInt, c: BigInt = 0): BigInt = {
-    var x = BigInt(3)
-    while (n%x != 0 && x*x < n)(x += 1)
-    if (x*x < n) largest(n/x,x) else n
-  }
-  println(largest(BigInt("600851475143")))
+  println(primes.take(1000).filter(600851475143L % _ == 0).max)
 }
