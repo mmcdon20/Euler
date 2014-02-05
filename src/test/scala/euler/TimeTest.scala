20837eec0ba4c282f3eb53a08b8386e3d1f3ef79
mmcdon20 <mmcdon20@live.com>
2014-02-05 01:22:05 -0600

    added test for problem 56

--- src/test/scala/euler/TimeTest.scala
+++ src/test/scala/euler/TimeTest.scala
@@ -70,5 +70,6 @@ class TimeTest {
   @Test def euler048() = time(Euler048)(9110846700L)
   @Test def euler050() = time(Euler050)(997651)
   @Test def euler052() = time(Euler052)(142857)
+  @Test def euler056() = time(Euler056)(972)
   @Test def euler067() = time(Euler067)(7273)
 }
