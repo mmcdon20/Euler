5de9d2ae48c88d5d9fceed717f380a187e300f9c
mmcdon20 <mmcdon20@live.com>
2014-02-06 18:24:16 -0600

    removed unnecessary set conversion in problem 50

--- src/main/scala/euler/Euler050.scala
+++ src/main/scala/euler/Euler050.scala
@@ -5,7 +5,6 @@ import util.Common._
 object Euler050 extends App {
   
   val ps = primes.takeWhile(_ < 1000000)
-  val primeSet = ps.toSet
 
   val upperLimit =
     Stream.from(1).find(ps.take(_).sum > 1000000).get -1
@@ -14,7 +13,7 @@ object Euler050 extends App {
     for (i <- upperLimit to 1 by -1) yield {
       ps.takeWhile(_ < 5000).sliding(i).foreach{ n =>
         val total = n.sum
-        if (total < 1000000 && primeSet(total))
+        if (total < 1000000 && ps.contains(total))
           return total
       }
     }
