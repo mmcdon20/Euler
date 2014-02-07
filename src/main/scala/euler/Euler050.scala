3fc2e0d33618af4de526fb33048911f883da6b06
mmcdon20 <mmcdon20@live.com>
2014-02-06 18:15:27 -0600

    added solution for problem 50

--- src/main/scala/euler/Euler050.scala
+++ src/main/scala/euler/Euler050.scala
@@ -4,9 +4,22 @@ import util.Common._
 
 object Euler050 extends App {
   
-  val primes = (1 to 500000).filter(isPrime(_))
-  
-  // incomplete
-  
-  //primes foreach println
+  val ps = primes.takeWhile(_ < 1000000)
+  val primeSet = ps.toSet
+
+  val upperLimit =
+    Stream.from(1).find(n => ps.take(n).sum > 1000000).get -1
+
+  def result:Int = {
+    for (i <- upperLimit to 1 by -1) yield {
+      ps.takeWhile(_ < 5000).sliding(i).foreach{ n =>
+        val total = n.sum
+        if (total < 1000000 && primeSet(total))
+          return total
+      }
+    }
+    0
+  }
+
+  println(result)
 }
