4e5b0d2b018ad10eba3507e8087e6ba4991b85da
mmcdon20 <mmcdon20@live.com>
2014-02-06 18:37:59 -0600

    combined double foreach into a single for loop in problem 50

--- src/main/scala/euler/Euler050.scala
+++ src/main/scala/euler/Euler050.scala
@@ -4,18 +4,13 @@ import util.Common._
 
 object Euler050 extends App {
   
-  val ps = primes.takeWhile(_ < 1000000)
+  val ps    = primes.takeWhile(_ < 1000000)
+  val limit = Stream.from(1).find(ps.take(_).sum > 1000000).get -1
 
-  val upperLimit =
-    Stream.from(1).find(ps.take(_).sum > 1000000).get -1
-
-  def result:Int = {
-    (upperLimit to 1 by -1).foreach{ i =>
-      ps.takeWhile(_ < 5000).sliding(i).foreach{ n =>
-        val total = n.sum
-        if (total < 1000000 && ps.contains(total))
-          return total
-      }
+  def result: Int = {
+    for (i <- limit to 1 by -1; n <- ps.takeWhile(_ < 5000).sliding(i)) {
+      val total = n.sum
+      if (total < 1000000 && ps.contains(total)) return total
     }
     0
   }
