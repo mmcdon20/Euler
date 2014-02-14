bb8691d40b0532542ff8d177b788bb9e4a1403d6
mmcdon20 <mmcdon20@live.com>
2014-02-13 18:46:54 -0600

    updated solution to problem 16

--- src/main/scala/euler/Euler016.scala
+++ src/main/scala/euler/Euler016.scala
@@ -1,6 +1,6 @@
 package euler
 
 object Euler016 extends App {
-  val x = BigInt(2).pow(1000).toString.toList.map(_.toInt-48).sum
-  println(x)
+  val result = BigInt(2).pow(1000).toString.map(_.asDigit).sum
+  println(result)
 }
