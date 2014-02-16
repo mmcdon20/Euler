fbe52f7da17d1c50299689d809a2354ca1968de0
mmcdon20 <mmcdon20@live.com>
2014-02-15 23:08:36 -0600

    updated solution for problem 5

--- src/main/scala/euler/Euler005.scala
+++ src/main/scala/euler/Euler005.scala
@@ -1,16 +1,16 @@
 package euler
 
 object Euler005 extends App {
-  def isDivisible(i:Int):Boolean = {
+  def isDivisible(i: Int): Boolean = {
     for (j <- 1 to 20)
       if (i % j != 0)
         return false
     true
   }
     
-  def findDivisible(i:Int):Int = i match {
-    case x if (isDivisible(x)) => x
-    case x                     => findDivisible(x+20)
+  def findDivisible(i: Int): Int = i match {
+    case x if isDivisible(x) => x
+    case x                   => findDivisible(x+20)
   }
     
   println(findDivisible(20))
