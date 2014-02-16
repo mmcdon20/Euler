3915a6c14c8f37c1c57269478083f2c6a4c4f565
mmcdon20 <mmcdon20@live.com>
2014-02-16 17:40:39 -0600

    updated solution to problem 9

--- src/main/scala/euler/Euler009.scala
+++ src/main/scala/euler/Euler009.scala
@@ -1,11 +1,11 @@
 package euler
 
 object Euler009 extends App {
-  def triplet() : Int = {
-    for (a <- 1 to 1000; b <- 1 to 1000; c <- (1 to 1000))
+  def triplet : Int = {
+    for (a <- 1 to 1000; b <- 1 to 1000; c <- 1 to 1000)
       if ((a < b && b < c) && (a*a + b*b == c*c) && (a + b + c == 1000))
         return a*b*c
-    return 0
+    0
   }
   println(triplet)
 }
