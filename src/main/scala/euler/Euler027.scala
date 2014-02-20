0499bc9e86a16241626212dc4d2322bf151f4541
mmcdon20 <mmcdon20@live.com>
2014-02-19 20:42:52 -0600

    improved efficiency of solution to problem 27

--- src/main/scala/euler/Euler027.scala
+++ src/main/scala/euler/Euler027.scala
@@ -1,19 +1,20 @@
 package euler
 
-import util.Implicit._
+import util.Common._
 import scala.math.abs
 
 object Euler027 extends App {
+  val primeSet = primes.takeWhile(_ < 1000).toSet
 
   def formula (a:Int, b:Int, n:Int) =
-    abs(n*n + a*n + b).isPrime
+    primeSet(abs(n*n + a*n + b))
 
   def comboLength(a:Int, b:Int, n:Int = 0): Int =
     if (formula(a,b,n)) comboLength(a,b,n+1)
     else n
 
   val combos =
-    for (a <- -999 to 999; b <- -999 to 999)
+    for (a <- -999 to 999; b <- a to 999)
       yield (comboLength(a,b),a*b)
 
   val result = combos.par.maxBy(_._1)
