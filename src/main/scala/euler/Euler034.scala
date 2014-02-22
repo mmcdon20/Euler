841e48f51e285f82a315f2d7bb9a133356410c60
mmcdon20 <mmcdon20@live.com>
2014-02-21 22:43:13 -0600

    improved performance of solution to problem 34

--- src/main/scala/euler/Euler034.scala
+++ src/main/scala/euler/Euler034.scala
@@ -1,9 +1,10 @@
 package euler
 
-import util.Implicit._
-
 object Euler034 extends App {
-  def isSumOfFactDigits(n: BigInt) = n == n.getDigits.map(_.factorial).sum
-  val numbers = (3 to 1000000).filter(isSumOfFactDigits(_))
+  def isSumOfFactDigits(n: Int) =
+    n == n.toString.foldLeft(0)((a,b) => a + factDigits(b.asDigit))
+
+  val factDigits = Array(1,1,2,6,24,120,720,5040,40320,362880)
+  val numbers    = (3 to 1000000).filter(isSumOfFactDigits)
   println (numbers sum)
 }
