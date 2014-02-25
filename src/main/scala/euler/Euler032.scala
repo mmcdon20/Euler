8986fcde5e8d151d3a8596da0e06ab3cf53f1aba
mmcdon20 <mmcdon20@live.com>
2014-02-24 23:02:21 -0600

    improved performance of solution to problem 32

--- src/main/scala/euler/Euler032.scala
+++ src/main/scala/euler/Euler032.scala
@@ -1,13 +1,11 @@
 package euler
 
-import util.Implicit._
-
 object Euler032 extends App {
+  val pans = "123456789".permutations.toSet
+
   def process(x:Int, y:Int) = 
-    if ((""+x+y+(x*y)).isPandigit(9))
-      x*y 
-    else 
-      0
+    if (pans(""+x+y+(x*y))) x*y
+    else 0
   
   val nums = for (i <- 1 to 100; j <- 101 to 10000) yield process(i,j)  
   println (nums.distinct.sum)
