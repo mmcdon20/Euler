aa1acbeefe82dabc5c7711f912350f5d1342d6ae
mmcdon20 <mmcdon20@live.com>
2014-02-27 02:43:31 -0600

    updated solution to problem 11

--- src/main/scala/euler/Euler011.scala
+++ src/main/scala/euler/Euler011.scala
@@ -5,21 +5,19 @@ import scala.util.Try
 
 object Euler011 extends App {
   val file = Source.fromFile("src/main/resources/11.txt").getLines()
-  val grid:List[Array[Int]] = file.toList.map(_.split(" ").map(_.toInt))
+  val grid: Array[Array[Int]] = file.toArray.map(_.split(" ").map(_.toInt))
 
   def get(x:Int, y:Int) = Try(grid(y)(x)).getOrElse(0)
 
-  def findHor(x:Int, y:Int) = (0 to 3).map(i => get(x+i,y+0)).product
-  def findVer(x:Int, y:Int) = (0 to 3).map(i => get(x+0,y+i)).product
-  def findDi1(x:Int, y:Int) = (0 to 3).map(i => get(x+i,y+i)).product
-  def findDi2(x:Int, y:Int) = (0 to 3).map(i => get(x+i,y-i)).product
-  
-  val hor = (for (i <- 0 to 19; j <- 0 to 19) yield findHor(i,j)).max
-  val ver = (for (i <- 0 to 19; j <- 0 to 19) yield findVer(i,j)).max
-  val di1 = (for (i <- 0 to 19; j <- 0 to 19) yield findDi1(i,j)).max
-  val di2 = (for (i <- 0 to 19; j <- 0 to 19) yield findDi2(i,j)).max
-  
-  println(hor::ver::di1::di2::Nil max)
+  def hor(x:Int, y:Int) = (0 to 3).map(i => get(x+i,y+0)).product
+  def ver(x:Int, y:Int) = (0 to 3).map(i => get(x+0,y+i)).product
+  def di1(x:Int, y:Int) = (0 to 3).map(i => get(x+i,y+i)).product
+  def di2(x:Int, y:Int) = (0 to 3).map(i => get(x+i,y-i)).product
+
+  val result = for (i <- 0 to 19; j <- 0 to 19)
+    yield List(hor(i,j),ver(i,j),di1(i,j),di2(i,j)).max
+
+  println(result max)
 }
 
 
