fc7a206911c42e25508ce31de58d29c63c470160
mmcdon20 <mmcdon20@live.com>
2014-02-28 22:57:32 -0600

    improved efficiency of solution to problem 5

--- src/main/scala/euler/Euler005.scala
+++ src/main/scala/euler/Euler005.scala
@@ -1,17 +1,11 @@
 package euler
 
 object Euler005 extends App {
-  def isDivisible(i: Int): Boolean = {
-    for (j <- 1 to 20)
-      if (i % j != 0)
-        return false
-    true
-  }
+  val range = 11 to 19
+
+  def findDivisible(n: Int = 20): Int =
+    if (range.forall(n%_ == 0)) n
+    else findDivisible(n+20)
     
-  def findDivisible(i: Int): Int = i match {
-    case x if isDivisible(x) => x
-    case x                   => findDivisible(x+20)
-  }
-    
-  println(findDivisible(20))
+  println(findDivisible())
 }
