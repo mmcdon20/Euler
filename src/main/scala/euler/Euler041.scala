7844b6ea0080257299f291a8bc334b99d85781ba
mmcdon20 <mmcdon20@live.com>
2014-03-01 23:14:37 -0600

    improved efficiency of solution to problem 41

--- src/main/scala/euler/Euler041.scala
+++ src/main/scala/euler/Euler041.scala
@@ -3,8 +3,13 @@ package euler
 import util.Implicit._
 
 object Euler041 extends App {
-  val range     = (123 to 321) ++ (1234 to 4321) ++ (12345 to 54321) ++ (123456 to 654321) ++ (1234567 to 7654321)
-  val pandigits = range.par.filter(_.isPandigit)
-  val primes    = pandigits.filter(_.isPrime)
-  println(primes max)
+  val range = (3 to 7)
+    .map("123456789".combinations)
+    .flatten.map(_.permutations)
+    .flatten.map(_.toInt)
+
+  val pandigitPrimes =
+    range.filter(n => n.isPandigit && n.isPrime)
+
+  println(pandigitPrimes max)
 }
