05884812a9262988e92e4a3c2f10fd3cec581393
mmcdon20 <mmcdon20@live.com>
2014-03-05 18:31:50 -0600

    improved efficiency of solution to problem 30

--- src/main/scala/euler/Euler030.scala
+++ src/main/scala/euler/Euler030.scala
@@ -1,15 +1,13 @@
 package euler
 
-import util.Implicit._
-
 object Euler030 extends App {
-  
-  def isSumOfDigitsOfPow(n:BigInt,p:Int):Boolean = 
-    n == n.getDigits.map(_.pow(p)).sum
-  
-  val pow5 = (2 to 1000000).filter(isSumOfDigitsOfPow(_,5))
+  val pows = Array(0,1,32,243,1024,3125,7776,16807,32768,59049)
+
+  def isSumOfDigitsPow5(n: Long) =
+    n == n.toString.foldLeft(0L)((a,b) => a + pows(b.asDigit))
+
+  val pow5 = (2 to 1000000).filter(isSumOfDigitsPow5(_))
   val answer = pow5.sum
   
   println(answer)
-
 }
