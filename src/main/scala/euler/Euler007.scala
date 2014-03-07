ddefef447492736c6c67c82759cf79aa2c1efec7
mmcdon20 <mmcdon20@live.com>
2014-03-06 20:10:00 -0600

    improved efficiency of solution to problem 7

--- src/main/scala/euler/Euler007.scala
+++ src/main/scala/euler/Euler007.scala
@@ -1,12 +1,7 @@
 package euler
 
-import util.Implicit._
+import util.Common._
 
 object Euler007 extends App {
-  def primeCount(n: BigInt,c:Int): BigInt = n match {
-    case x if c == 10001 => x-1
-    case x if x.isPrime  => primeCount(x+1,c+1)
-    case x               => primeCount(x+1,c)
-  }
-  println(primeCount(1,0))
+  println(primes(10000))
 }
