c8e0c4630be062f5f5d5b635cd9c69fdc56395b8
mmcdon20 <mmcdon20@live.com>
2014-03-09 22:20:40 -0500

    updated solution to problem 8

--- src/main/scala/euler/Euler008.scala
+++ src/main/scala/euler/Euler008.scala
@@ -1,11 +1,10 @@
 package euler
 
 import scala.io.Source
-import util.Implicit._
 
 object Euler008 extends App {
   val file = Source.fromFile("src/main/resources/8.txt")
-  val nums = file.toList.filter(_.isDigit).map(_.toDigit)
+  val nums = file.toList.filter(_.isDigit).map(_.asDigit)
   val gp = nums.sliding(5).map(_.product).max
   println(gp)
 }
