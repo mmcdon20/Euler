b6491b56ae0b2bc7ad020505d0c2ac2609f68482
mmcdon20 <mmcdon20@live.com>
2014-04-01 14:39:58 -0500

    improved efficiency of solution to problem 21

--- src/main/scala/euler/Euler021.scala
+++ src/main/scala/euler/Euler021.scala
@@ -6,7 +6,7 @@ object Euler021 extends App {
     (1 to n/2).filter(n % _ == 0).sum
 
   def amicable(a: Int)(b: Int = d(a)) =
-    d(a) == b && d(b) == a && a != b
+    d(b) == a && a != b
 
   val total =
     (1 until 10000).par.filter(amicable(_)( )).sum
